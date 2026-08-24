ce467782272123dc50717e37bbbaf0443c3db962
Kenneth Brook <kenneth.brook@hotmail.com>
2026-08-24 12:21:01 -0400

    Correct Insider South branding

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,7 +8,7 @@ const LandingPage = () => {
     <main className="landing-page">
       <section className="landing-page__brand-panel">
         <div className="landing-page__brand">
-          <div className="landing-page__inside">INSIDE</div>
+          <div className="landing-page__inside">INSIDER</div>
           <div className="landing-page__south">SOUTH</div>
 
           <div className="landing-page__city">COLUMBUS</div>
