c9d21615c5d28a629dec5951964b8cb7ae3a0da0
Kenneth Brook <kenneth.brook@hotmail.com>
2026-08-26 14:59:42 -0400

    Use Visit Georgia logo image on landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,11 +8,11 @@ const LandingPage = () => {
     <main className="landing-page">
       <section className="landing-page__brand-panel">
         <div className="landing-page__brand">
-          <div className="landing-page__inside">VISIT</div>
-          <div className="landing-page__south">GEORGIA</div>
-
-          <div className="landing-page__city">COLUMBUS</div>
-          <div className="landing-page__location">GEORGIA'S RIVER CITY</div>
+          <img
+            src={`${process.env.PUBLIC_URL}/images/visit-georgia.png`}
+            alt="Visit Georgia Columbus"
+            className="landing-page__brand-image"
+          />
         </div>
 
         <div className="landing-page__guide-copy">
