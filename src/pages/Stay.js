c052c8adfaff1815fb1b2964b195643f94413c94
Kenneth Brook <kenneth.brook@hotmail.com>
2026-09-18 09:42:39 -0400

    Show distance on stay cards

--- src/pages/Stay.js
+++ src/pages/Stay.js
@@ -10,6 +10,7 @@ import { useViewMode } from '../hooks/ViewModeContext';
 import MapView from './components/MapView';
 import { useNavigate } from 'react-router-dom';
 import DetailViewCard from './components/DetailViewCard';
+import DistanceLabel from './components/DistanceLabel';
 import ShareModal from './components/ShareModal';
 
 const Stay = ({ pageTitle }) => {
@@ -82,6 +83,7 @@ const Stay = ({ pageTitle }) => {
     <div className="two-column-layout">
       {sortedStayData.map((item) => (
         <div key={item.id} className="content-item">
+          <DistanceLabel item={item} />
           <h2>{item.name}</h2>
           <div className="content-box">
             <div className="box-top">
