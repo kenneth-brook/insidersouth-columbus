282ea54d6868395c78d35e35118a0867b9e4a785
Kenneth Brook <kenneth.brook@hotmail.com>
2026-09-25 11:44:21 -0400

    Show feature buttons on location detail view

--- src/pages/components/DetailView.js
+++ src/pages/components/DetailView.js
@@ -15,6 +15,7 @@ import { ReactComponent as BackArrow } from '../../assets/icos/back-arrow.svg';
 import Header from './Header';
 import Footer from './Footer';
 import MapView from './MapView';
+import PublicationButton from './PublicationButton';
 import '../../sass/componentsass/DetailView.scss';
 import { useViewMode } from '../../hooks/ViewModeContext';
 import ShareModal from './ShareModal';
@@ -245,6 +246,7 @@ const DetailView = () => {
                 </div>
               )}
               <p dangerouslySetInnerHTML={{ __html: item.description }}></p>
+              <PublicationButton item={item} />
             </div>
 
             {orientation === 'landscape-primary' ? (
